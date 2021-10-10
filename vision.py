1752865942fcd7781fbd8cb5f2516eb67c325a28
Nathan Willer <nathanielwiller93@gmail.com>
2021-10-10 16:15:53 -0400

    we got pictures, baby

---
+++ vision.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+import sys
+
+# 0 reads the image in greyscale
+with_coin = cv2.imread('images/with_coin.png', 0)
+without_coin = cv2.imread('images/without_coin.png', 0)
+template = cv2.imread('images/template.png', 0)
+
+np.set_printoptions(threshold=sys.maxsize)
+print(with_coin.shape)
+print(without_coin.shape)
+print(template.shape)
+#print(template)
